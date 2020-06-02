a9be5034ad4c433212abd6cf31042bb4d2fedaed
Rodrigo Gomes <rodrigogomes51@users.noreply.github.com>
2020-06-01 23:58:19 -0300

    bug loading fix

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -231,25 +231,25 @@ jQuery(function ($) {
 
 
 
-    // -------------------------------------------------------------
-    // Vidio auto play
-    // -------------------------------------------------------------
-    (function () {
+    // // -------------------------------------------------------------
+    // // Vidio auto play
+    // // -------------------------------------------------------------
+    // (function () {
     
-    /* Vimeo API: http://developer.vimeo.com/player/js-api */
+    // /* Vimeo API: http://developer.vimeo.com/player/js-api */
     
-        var iframe = document.getElementById('nofocusvideo');
-        // $f == Froogaloop
-        var player = $f(iframe);
+    //     var iframe = document.getElementById('nofocusvideo');
+    //     // $f == Froogaloop
+    //     var player = $f(iframe);
 
-        $('.modal').on('hidden.bs.modal', function () {
-        player.api('pause');
-        })
+    //     $('.modal').on('hidden.bs.modal', function () {
+    //     player.api('pause');
+    //     })
 
-        $('.modal').on('shown.bs.modal', function () {
-        player.api('play');
-        })
-    }());
+    //     $('.modal').on('shown.bs.modal', function () {
+    //     player.api('play');
+    //     })
+    // }());
 
 
 
