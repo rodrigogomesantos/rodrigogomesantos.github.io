d1bea516f7760a2d74f22b885661217a84c91b34
Rodrigo Gomes <rodrigogomes51@users.noreply.github.com>
2020-06-01 23:26:22 -0300

    Start

---
+++ assets/js/magnific.js
@@ -0,0 +1,13 @@
+ $(document).ready(function() {
+
+        $('.simple-ajax-popup-align-top').magnificPopup({
+          type: 'ajax',
+          alignTop: true,
+          overflowY: 'scroll' // as we know that popup content is tall we set scroll overflow by default to avoid jump
+        });
+
+        $('.simple-ajax-popup').magnificPopup({
+          type: 'ajax'
+        });
+        
+      });
